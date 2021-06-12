b76b0c33401844882fcf01db4d2a9494adeb5696
lokesh bajracharya <lokesh.bajracharya55@gmail.com>
2021-06-13 03:03:54 +0545

    serve react from backend

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 require("dotenv").config();
+
+const path = require('path');
 const db = require('./config/operationdb')
 const sqlmodels = require('./models')
 //bring routes
@@ -23,7 +25,19 @@ app.use(cookieParser());
 
 //allow cors on development
 if (process.env.NODE_ENV == "development") {
+
   app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
+
+} else if (process.env.NODE_ENV === 'production') {
+  // Serve a static assets if in productions
+  // Set a static folder
+  app.use(express.static(path.join(__dirname, "../frontend/build")))
+  // Send all requests in build index
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'));
+  });
+
+  console.log("Sent file to build index.html")
 }
 
 //routes middleware
@@ -32,7 +46,7 @@ app.use("/api", apiRoutes);
 //port
 const port = process.env.PORT || 5000;
 
-sqlmodels.sequelize.sync().then(req=> {
+sqlmodels.sequelize.sync().then(req => {
   app.listen(port, () => {
     console.log(`Your server is running on port ${port}`);
   });
